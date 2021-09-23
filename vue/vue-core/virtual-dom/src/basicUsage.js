a3740999c0f05c84f4735366bbb750cc2468b5aa
askwuxue <972523419@qq.com>
2021-09-23 21:41:16 +0800

    update

--- vue/vue-core/virtual-dom/src/basicUsage.js
+++ vue/vue-core/virtual-dom/src/basicUsage.js
@@ -12,26 +12,37 @@ const patch = init([
   eventListenersModule
 ]);
 
-let vNode = h('div#container.cls', { hook: {
-  init (vnode) {
-    console.log('vnode: ', vnode)
-  },
-  create (emptyNode, vnode) {
-    console.log('emptyNode: ', emptyNode)
-    console.log('vnode: ', vnode)
-  }
-}}, 'Hello World')
-// let vNode = h('div#container.cls', {}, [
-//   h('p', 'this is old contents')
-// ])
+// let vNode = h('div#container.cls', { hook: {
+//   init (vnode) {
+//     console.log('vnode: ', vnode)
+//   },
+//   create (emptyNode, vnode) {
+//     console.log('emptyNode: ', emptyNode)
+//     console.log('vnode: ', vnode)
+//   }
+// }}, 'Hello World')
+
+let vNode = h('div#container.cls', {}, [
+  h('ul', {}, [
+    h('li', '首页'),
+    h('li', '微博'),
+    h('li', '视频'),
+  ])
+])
 
 let dom = document.querySelector('#app')
 
 let oldDom = patch(dom, vNode)
 
-// vNode = h('div#container.xxx', 'Hello snabbdom')
+vNode = h('div#container.cls', {}, [
+  h('ul', {}, [
+    h('li', '首页'),
+    h('li', '视频'),
+    h('li', '微博'),
+  ])
+])
 
-// patch(oldDom, vNode)
+patch(oldDom, vNode)
 
 
 // setTimeout(() => {
