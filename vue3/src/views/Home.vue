602dd4ed6e1609f8c6bdabfdff3ee89ede2be6d8
askwuxue <972523419@qq.com>
2021-10-19 21:46:14 +0800

    init

---
+++ vue3/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+  },
+});
+</script>
