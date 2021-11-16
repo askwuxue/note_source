a6a25a78e6ccd28dad84d1e787a73421620bdb8d
askwuxue <972523419@qq.com>
2021-11-16 23:59:36 +0800

    import cli helper commander.js

--- askwu/index.js
+++ askwu/index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
-console.log('cli working....')
+// console.log('cli working....')
+const { program } = require('commander');
+const packageJson = require('./package.json')
+const version = packageJson.version
+program.version(version);
+program.parse()
