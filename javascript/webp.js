dac2802f5415f954d6ab4a0b921d66ef6d43f9c5
askwuxue <972523419@qq.com>
2022-04-01 23:14:15 +0800

    update webp

---
+++ javascript/webp.js
@@ -0,0 +1,11 @@
+var isSupportWebp = function () {
+  try {
+    return (
+      document.createElement("canvas").toDataURL("image/webp", 0.5).indexOf("data:image/webp") === 0
+    );
+  } catch (err) {
+    return false;
+  }
+};
+
+isSupportWebp();
