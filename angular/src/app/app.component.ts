c6a5c6ac12dea2ba5c9664bfc7908fd2159efcf4
askwuxue <972523419@qq.com>
2022-04-17 23:31:31 +0800

    update comments

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+// AppComponent 本来就是一个普通的typescript类，但是上面的组件元数据装饰器告诉Angular，
+// AppComponent是一个组件，需要把一些元数据加到这个类上，Angular就会把AppComponent当组件来处理
 export class AppComponent {
   title = 'angular';
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Angular 应用是模块化的，它拥有自己的模块化系统，称作 NgModule。每个 Angular 应用都至少有一个 NgModule 类，也就是根模块，在app.module.ts文件中，这个根模块就可以启动你的应用。
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  // declarations（可声明对象表） —— 那些属于本 NgModule 的组件、指令、管道
+  declarations: [AppComponent],
+  // exports（导出表） —— 那些能在其它模块的组件模板中使用的可声明对象的子集。
+  // imports（导入表） —— 导入其他模块
+  imports: [BrowserModule, AppRoutingModule],
+  // providers —— 依赖注入
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap —— 设置根组件
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
